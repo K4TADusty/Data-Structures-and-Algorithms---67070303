1b48af6afc4364a1608c052d1c2bf41be96c15d7
Kimirei <67070303@kmitl.ac.th>
2024-12-06 12:19:33 +0700

    Lab 01.02 - Max…Min…Avg.py

--- Lab 01.02 - Max…Min…Avg.py
+++ Lab 01.02 - Max…Min…Avg.py
@@ -1,8 +1,13 @@
 """Lab 01.02 - Max…Min…Avg"""
 import json
 my_list = json.loads(input())
-max_value = max(my_list)
-min_value = min(my_list)
-avg_value = round(sum(my_list)/len(my_list),2)
-result = (max_value,min_value,avg_value)
+max_value = my_list[0]
+min_value = my_list[0]
+for num in my_list:
+    if num > max_value:
+        max_value = num
+    if num < min_value:
+        min_value = num
+avg_value = round(sum(my_list) / len(my_list), 2)
+result = (max_value, min_value, avg_value)
 print(result)
